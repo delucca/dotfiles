185d0bff6e7a5eea391d8f3f244ba07bd8a38465
delucca <delucca@pm.me>
2021-06-04 17:49:33 -0300

    updating

--- shell/aliases/chia.sh
+++ shell/aliases/chia.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-alias hs='harvester-status'
-alias hsff='ssh ff-farmer@10.0.0.12 harvester-status'
-alias hsblz='ssh blz-farmer@10.0.0.11 harvester-status'
+alias hs='harvester status'
+alias hsff='ssh ff-farmer@10.0.0.12 "harvester status"'
+alias hsblz='ssh blz-farmer@10.0.0.11 "harvester status"'
