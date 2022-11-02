a72275ea88ad6ef8fceeecbb60d33702444b1108
delucca <gpg@delucca.dev>
2022-11-02 19:14:26 -0300

    updating

--- configs/chezmoi/private_dot_config/better-vim/configs/dap.lua
+++ configs/chezmoi/private_dot_config/better-vim/configs/dap.lua
@@ -54,12 +54,12 @@ for _, language in ipairs({ "typescript", "javascript" }) do
 	}
 end
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	dap_ui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dap_ui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dap_ui.close()
-end
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+-- 	dap_ui.open()
+-- end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+-- 	dap_ui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+-- 	dap_ui.close()
+-- end
