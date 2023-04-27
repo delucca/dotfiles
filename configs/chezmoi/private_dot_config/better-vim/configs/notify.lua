47ff01819ee64cdf8320067010abd71e83dc935f
delucca <gpg@delucca.dev>
2023-04-27 18:54:52 -0300

    update

--- configs/chezmoi/private_dot_config/better-vim/configs/notify.lua
+++ configs/chezmoi/private_dot_config/better-vim/configs/notify.lua
@@ -3,4 +3,16 @@ if not status_ok then
 	return
 end
 
+local function split_length(text, length)
+	local lines = {}
+	local next_line
+	while true do
+		if #text == 0 then
+			return lines
+		end
+		next_line, text = text:sub(1, length), text:sub(length)
+		lines[#lines + 1] = next_line
+	end
+end
+
 vim.notify = notify
