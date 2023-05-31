00e8df80603002e7b6ea85d2fff8157dba484d99
delucca <gpg@delucca.dev>
2023-05-31 18:22:56 -0300

    update

--- configs/chezmoi/private_dot_config/better-vim/better-vim.lua
+++ configs/chezmoi/private_dot_config/better-vim/better-vim.lua
@@ -2,8 +2,8 @@ return {
 	lsps = {
 		pyright = {},
 	},
+	eslint = {},
 	formatters = {
-		eslint = {},
 		black = {},
 		stylua = {},
 	},
@@ -18,14 +18,16 @@ return {
 				"Rename in file",
 			},
 			["grR"] = { "<cmd>lua require'spectre'.open_visual({ select_word = true })<cr>", "Rename in project" },
-			["<M-e>"] = { "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree" },
-			["<M-g>"] = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Toggle Lazygit window" },
+			["<m-e>"] = { "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree" },
+			["<m-g>"] = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Toggle Lazygit window" },
 			["|"] = { "<cmd>vsplit<cr>", "Create a new vertical window" },
 			["-"] = { "<cmd>split<cr>", "Create a new horizontal window" },
 			["<leader>vl"] = { "<cmd>VimtexCompile<cr>", "Compile current TeX file" },
 			["<leader>vv"] = { "<cmd>VimtexView<cr>", "View current TeX file" },
 			["<c-j>"] = { "<c-n>", "Navigate to next on autocomplete", mode = { "i" }, remap = true },
 			["<c-k>"] = { "<c-p>", "Navigate to previous on autocomplete", mode = { "i" }, remap = true },
+			["<m-p>"] = { "<plug>(VM-Add-Cursor-Up)", "Add a new cursor up" },
+			["<m-n>"] = { "<plug>(VM-Add-Cursor-Down)", "Add a new cursor down" },
 		},
 	},
 	plugins = {
@@ -41,15 +43,13 @@ return {
 		"pwntester/octo.nvim",
 		"akinsho/toggleterm.nvim",
 		"lervag/vimtex",
-		-- "mg979/vim-visual-multi",
-		-- "gelguy/wilder.nvim",
+		"mg979/vim-visual-multi",
 	},
 	hooks = {
 		after_setup = function()
 			require("better-vim.configs.treesitter")
 			require("better-vim.configs.toggleterm")
-			-- require("better-vim.configs.vim-visual-multi")
-			-- require("better-vim.configs.wilder")
+			require("better-vim.configs.vim-visual-multi")
 		end,
 	},
 }
--- configs/chezmoi/private_dot_config/better-vim/configs/vim-visual-multi.lua
+++ configs/chezmoi/private_dot_config/better-vim/configs/vim-visual-multi.lua
@@ -1,7 +1,3 @@
-vim.g.VM_leader = "<Space>"
 vim.g.VM_show_warnings = 0
+vim.g.VM_silent_exit = 1
 vim.g.VM_theme = "ocean"
-
-vim.g.VM_maps = {}
-vim.g.VM_maps["Add Cursor Up"] = '<C-M-k>'
-vim.g.VM_maps["Add Cursor Down"] = '<C-M-j>'
